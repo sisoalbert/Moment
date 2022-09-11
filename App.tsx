a887f1726282712a1a9962801fbef85649a6714f
sisoalbert <sisoalbert@yahoo.com>
2022-09-11 10:40:20 +0200

    working Fetch

--- App.tsx
+++ App.tsx
@@ -1,120 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, Text, View} from 'react-native';
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const getTeams = async () => {
+    try {
+      const response = await fetch(
+        // 'https://v1.formula-1.api-sports.io/status'
+        // 'https://api-formula-1.p.rapidapi.com/teams',
+        'https://v1.formula-1.api-sports.io/teams',
+        {
+          method: 'GET',
+          headers: {
+            'x-rapidapi-host': 'api-formula-1.p.rapidapi.com',
+            'x-rapidapi-key': 'API-KEY',
+          },
+        },
+      );
+      const json = await response.json();
+      console.log('====================================');
+      console.log(json.response);
+      setData(json.response);
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+      console.log('====================================');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+      console.log('====================================');
+      console.log('Everything went well!');
+      console.log('====================================');
+    }
+  };
+
+  useEffect(() => {
+    getTeams();
+  }, []);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex: 1, padding: 24}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({id}, index) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.name}, {item.base}
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
 };
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
---
+++ Readme.md
@@ -0,0 +1,7 @@
+Produce a simple React Native app that retrieves and lists the players/drivers for two of your
+favourite teams - the results should be displayed in a two column grid layout and we should
+be able to view details of each player when tapping on them in the list view.
+Some bonus points if we can filter between the teams, see images of the players/drives and
+if we are are able to favourite players/drivers (this can be persisted on the device)
+You can use the API’s provided via https://api-sports.io/
+(i.e - https://api-sports.io/documentation/formula-1/v1)
