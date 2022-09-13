64e493d68ec3a877b142686e950a32b8e24ac174
sisoalbert <sisoalbert@yahoo.com>
2022-09-13 12:28:14 +0200

    working persist

--- App.tsx
+++ App.tsx
@@ -3,12 +3,20 @@ import SafeAreaView from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
 
+// redux persist
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
+
+let persistor = persistStore(store);
+
 import RootApp from './src/navigation/RootApp';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <RootApp />
+      <PersistGate loading={null} persistor={persistor}>
+        <RootApp />
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,27 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import thunk from 'redux-thunk';
+
 import {bookSlice} from './favSlice';
 
+const reducers = combineReducers({
+  book: bookSlice.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['book'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    book: bookSlice.reducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
